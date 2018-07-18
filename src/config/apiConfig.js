887bfaaff16d88ab76cf4db9e6997b3eb84f8be9
haylhf <haylhf@163.com>
2018-07-18 19:00:57 +0800

    安装调试页面布局

--- src/config/apiConfig.js
+++ src/config/apiConfig.js
@@ -1,67 +1,15 @@
 const APIConfig = {
 
 	request_server_url: '/api/',  // for axios ajax url
-	ajax_header_token: 'security_token',    // for axios request header
-	ajax_header_role: 'security_role',      // for axios request header
 
+	EveryPageNum: 10, //每页显示记录数
 
-	/*
-	 ****************************************************************************************
-	 ****************************************************************************************
-	 *               response data format
-	 *              (json)
-	 **************************************
-	 */
-	response_token: 'token',    // 可能不需要， 直接在 response_data中传递
-	response_status: 'status',  // *必须*   执行状态 [success, error]
-	response_data: 'data',      // *必须*   应该是  json 格式
-	/*
-	 ****************************************************************************************
-	 ****************************************************************************************
-	 *               ajax (axios)
-	 *                    # 除了 base_url 其他变量 计划放到 header中传递给后端
-	 *                    # 这样就可以 直接在 utils/fetch.js 中统一处理所有 操作请求， 验证操作有效性
-	 **************************************
-	 */
-
-
-	/*
-	 ****************************************************************************************
-	 ****************************************************************************************
-	 *               cookie (js-cookie)
-	 **************************************
-	 */
-	cookie_user_name: 'user_name',
-	cookie_user_role: 'user_role',
-	cookie_user_id: 'user_id',
-	cookie_user_session: 'user_session',
-	cookie_user_token: 'user_token',
-	/*
-	 ****************************************************************************************
-	 ****************************************************************************************
-	 *               store_user (vuex)
-	 **************************************
-	 */
-	store_user_token: 'user_token', // for store to save token
-	store_user_role: 'user_role',   // for store to save role
-	store_user_name: 'user_name',
-	store_user_id: 'user_id',
-	store_user_session: 'user_session',
-	/*
-	 ****************************************************************************************
-	 ****************************************************************************************
-	 *               store_getters (vuex)
-	 **************************************
-	 */
-	store_getters_username: 'user_name',
-	store_getters_id: 'user_id',
-	store_getters_role: 'user_role',
-	store_getters_session: 'user_session',
-	store_getters_token: 'user_token',
-	store_getters_table_count: 'record_total',
-	store_getters_table_records: 'table_records',
-	store_getters_table_page_size: 'table_page_size',
-	store_getters_table_exec_result: 'table_exec_result',
+	//机器状态
+	MachineStatusList: [
+		{value: 0, name: '待分配'},
+		{value: 2, name: '待调试'},
+		{value: 1, name: '已安装'},
+	],
 }
 
 export  {APIConfig}
--- src/views/install_machine/install_home.vue
+++ src/views/install_machine/install_home.vue
@@ -1,23 +1,375 @@
-<template>
-  <div class='install_home_div'>
-       install_home:'调试主页',
-  </div>
-</template>
+<template >
+  <div class="install_home_div" >
+      <div >
+        <el-col class="well well-lg" style="background-color: white;" >
+            <el-form :model="condition" label-position="right" label-width="90px" >
+                <el-row >
+                    <el-col :span="4" >
+                        <el-form-item label="机器编号:" >
+                            <el-input v-model="condition.nameplate"
+                                      placeholder="订单号"
+                                      auto-complete="off" ></el-input >
+                        </el-form-item >
+                    </el-col >
+                     <el-col :span="4" >
+                        <el-form-item label="订单编号:" >
+                            <el-input v-model="condition.orderNum"
+                                      placeholder="订单号"
+                                      auto-complete="off" ></el-input >
+                        </el-form-item >
+                    </el-col >
+                    <el-col :span="4" >
+                        <el-form-item label="机型:" >
+                            <el-select v-model="condition.machineType" clearable >
+                                <el-option
+		                                v-for="item in allMachineType"
+		                                :value="item.value"
+		                                :label="item.name" >
+                                </el-option >
+                            </el-select >
+                        </el-form-item >
+                    </el-col >
+	                <el-col :span="2" :offset="1" >
+	                    <el-form-item label="显示代理商:" >
+                             <el-switch
+		                             v-model="condition.isAgent"
+		                             active-text="是"
+		                             inactive-text="否" >
+							 </el-switch >
+	                    </el-form-item >
+                    </el-col >
+	                <el-col :span="3" v-show="condition.isAgent">
+	                    <el-form-item label="" >
+		                    <el-input v-model="condition.agent"
+		                              placeholder="代理商"
+		                              auto-complete="off" ></el-input >
+	                    </el-form-item >
+                    </el-col >
+                </el-row >
+                <el-row >
+                     <el-col :span="4" >
+                        <el-form-item label="完成状态:" >
+                            <el-select v-model="condition.status" clearable >
+                                <el-option
+		                                v-for="item in statusList"
+		                                :value="item.value"
+		                                :label="item.name" >
+                                </el-option >
+                            </el-select >
+                        </el-form-item >
+                    </el-col >
+	                <el-col :span="4" >
+                        <el-form-item label="客户:" >
+                            <el-input v-model="condition.customer"
+                                      placeholder="客户"
+                                      auto-complete="off" ></el-input >
+                        </el-form-item >
+                    </el-col >
 
-<script>
-import Chart from '@/components/Charts/keyboard'
+                    <el-col :span="8" >
+                        <el-form-item label="日期:" >
+                            <el-date-picker
+		                            v-model="condition.selectDate"
+		                            type="daterange"
+		                            align="left"
+		                            unlink-panels
+		                            range-separator="—"
+		                            start-placeholder="开始日期"
+		                            end-placeholder="结束日期"
+		                            :picker-options="pickerOptions" >
+                            </el-date-picker >
+                        </el-form-item >
+                    </el-col >
+	                <el-col :span="2" :offset="6" >
+                        <el-button
+		                        icon="el-icon-search"
+		                        size="normal"
+		                        type="primary"
+		                        @click="search" >查询
+                        </el-button >
+                    </el-col >
+                </el-row >
+            </el-form >
+            <el-table
+		            v-loading="loadingUI"
+		            element-loading-text="获取数据中..."
+		            :data="tableData"
+		            border
+		            empty-text="暂无数据..."
+		            ref="singleTable"
+		            highlight-current-row
+		            show-overflow-tooltip="true"
+		            style="width: 100%; " >
+                <el-table-column
+		                width="75"
+		                align="center"
+		                label="序号" >
+                    <template scope="scope" >
+                        {{scope.$index+startRow}}
+                    </template >
+                </el-table-column >
 
+                <el-table-column
+		                align="center"
+		                prop="nameplate"
+		                label="机器编号" >
+                </el-table-column >
+                <el-table-column
+		                align="center"
+		                prop="machineType"
+		                label="机型" >
+                    <template scope="scope" >
+                        <div >
+                            {{scope.row.machineType|filterMachineType}}
+                        </div >
+                    </template >
+                </el-table-column >
+                <el-table-column label="订单号"
+                                 align="center"
+                                 prop="orderNum" >
+                    <template scope="scope" >
+                        <div >
+                            {{scope.row.orderNum}}
+                        </div >
+                    </template >
+                </el-table-column >
+                <el-table-column
+		                align="center"
+		                prop="customer"
+		                label="客户" >
+                </el-table-column >
+                <el-table-column
+		                align="center"
+		                prop="customer"
+		                label="代理商" >
+                </el-table-column >
+                <el-table-column
+		                align="center"
+		                label="安装状态" >
+                    <template scope="scope" >
+                        <div v-if="scope.row.status==0"
+                             style="color: #686868" >
+                            {{scope.row.status|filterStatus}}
+                        </div >
+                        <div v-if="scope.row.status==1"
+                             style="color: #8b6c0e" >
+                            {{scope.row.status|filterStatus}}
+                        </div >
+                    </template >
+                </el-table-column >
+                <el-table-column
+		                align="center"
+		                prop="processCreateTime"
+		                label="交货日期" >
+                    <template slot-scope="scope" >
+                        <span >
+                            {{(scope.row.processCreateTime)|filterDateString}}
+                        </span >
+                    </template >
+                </el-table-column >
+	            <el-table-column
+			             align="center"
+			             prop="processCreateTime"
+			             label="出厂日期" >
+                    <template slot-scope="scope" >
+                        <span >
+                            {{(scope.row.processCreateTime)|filterDateString}}
+                        </span >
+                    </template >
+                </el-table-column >
+	            <el-table-column
+			            align="center"
+			            prop="processCreateTime"
+			            label="完成日期" >
+                    <template slot-scope="scope" >
+                        <span >
+                            {{(scope.row.processCreateTime)|filterDateString}}
+                        </span >
+                    </template >
+                </el-table-column >
+
+                <el-table-column width="200"
+                                 label="操作" align="center" >
+                    <template scope="scope" style="text-align: center" >
+                        <el-tooltip placement="left" content="详情" >
+                            <el-button
+		                            size="mini"
+		                            type="primary"
+		                            icon="el-icon-view"
+		                            @click="editWithItem(scope.row)" >
+                            </el-button >
+                        </el-tooltip >
+	                    <el-tooltip placement="left" content="分配" >
+                            <el-button
+		                            size="mini"
+		                            type="primary"
+		                            icon="el-icon-view"
+		                            @click="editWithItem(scope.row)" >查看
+                            </el-button >
+                        </el-tooltip >
+                    </template >
+                </el-table-column >
+            </el-table >
+            <div class="block" style="text-align: center; margin-top: 20px" >
+                <el-pagination
+		                background
+		                @current-change="handleCurrentChange"
+		                :current-page="currentPage"
+		                :page-size="pageSize"
+		                layout="total, prev, pager, next, jumper"
+		                :total="totalRecords" >
+                </el-pagination >
+            </div >
+	        <!--<el-dialog title="确认取消机器" :visible.sync="confirmCancelDialog" width="30%"-->
+	        <!--:modal="false" >-->
+	        <!--<span >确定要取消机器编号为 [<b style="color: red;font-size: 18px" >{{selectedItem.nameplate}}</b >] 吗？</span >-->
+	        <!--<span slot="footer" class="dialog-footer" >-->
+	        <!--<el-button @click="confirmCancelDialog = false"-->
+	        <!--icon="el-icon-close" >取 消</el-button >-->
+	        <!--<el-button type="primary" @click="onConfirmCancel"-->
+	        <!--icon="el-icon-check" >确 定</el-button >-->
+	        <!--</span >-->
+	        <!--</el-dialog >-->
+        </el-col >
+
+    </div >
+  </div >
+</template >
+
+<script >
+import {APIConfig} from '@/config/apiConfig'
+import {Loading} from 'element-ui';
 export default {
 	name: 'install_home',
-	components: { Chart }
-}
-</script>
+	components: {},
+	data() {
+		return {
+			tableData: [],
+			//分页
+			pageSize: APIConfig.EveryPageNum,//每一页的num
+			currentPage: 1,
+			startRow: 0,
+			totalRecords: 0,
+			statusList: APIConfig.MachineStatusList,
+			condition: {
+				nameplate: '',
+				orderNum: '',
+				machineType: '',
+				customer: '',
+				isAgent: true,
+				agent: '',
+				status: '',
+				selectDate: '',
+			},
+			allMachineType: [],
+			allRoles: [],
+			loadingUI: false,
+			pickerOptions: {
+				shortcuts: [{
+					text: '最近一周',
+					onClick(picker) {
+						const end = new Date();
+						const start = new Date();
+						start.setTime(start.getTime() - 3600 * 1000 * 24 * 7);
+						picker.$emit('pick', [start, end]);
+					}
+				}, {
+					text: '最近一个月',
+					onClick(picker) {
+						const end = new Date();
+						const start = new Date();
+						start.setTime(start.getTime() - 3600 * 1000 * 24 * 30);
+						picker.$emit('pick', [start, end]);
+					}
+				}, {
+					text: '最近三个月',
+					onClick(picker) {
+						const end = new Date();
+						const start = new Date();
+						start.setTime(start.getTime() - 3600 * 1000 * 24 * 90);
+						picker.$emit('pick', [start, end]);
+					}
+				}]
+			},
+		}
+	},
+	mounted() {
 
-<style scoped>
-.install_home_div{
+	},
+	filters: {
+		filterDateString(strDate)
+		{
+			var resDate = new Date(strDate);
+			return resDate.format("yyyy-MM-dd");
+		},
+
+		filterStatus(id)
+		{
+			var result = this.statusList[0].name;
+			for (var i = 0; i < this.statusList.length; i++) {
+				if (id == this.statusList[i].value) {
+					result = this.statusList[i].name;
+					break;
+				}
+			}
+			return result;
+		},
+		filterMachineType(id)
+		{
+			var result = '';
+			for (var i = 0; i < this.allMachineType.length; i++) {
+				if (id == this.allMachineType[i].id) {
+					result = this.allMachineType[i].name;
+					break;
+				}
+			}
+			return result;
+		},
+	},
+	methods: {
+		handleCurrentChange(val) {
+			this.currentPage = val;
+			this.search();
+		},
+		search() {
+			this.onSearchDetailData();
+		},
+		onSearchDetailData()
+		{
+			var condition = {
+				orderNum: this.condition.orderNum.trim(),
+				nameplate: this.condition.nameplate.trim(),
+				query_start_time: '',
+				query_finish_time: '',
+				status: this.condition.status,
+				is_fuzzy: true,
+				page: this.currentPage,
+				size: this.pageSize
+			};
+			if (this.condition.selectDate != null && this.condition.selectDate.length > 0) {
+				condition.query_start_time = this.condition.selectDate[0].format("yyyy-MM-dd");
+				condition.query_finish_time = this.condition.selectDate[1].format("yyyy-MM-dd");
+			}
+
+		},
+		editWithItem(item)
+		{
+
+
+		},
+	}
+}
+</script >
+
+<style scoped >
+.install_home_div {
 	position: relative;
 	padding: 20px;
 	width: 100%;
 	height: 85vh;
 }
-</style>
+
+.el-select {
+		width: 100%;
+}
+</style >
